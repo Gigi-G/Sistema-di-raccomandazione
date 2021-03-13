7800396bda7e923ac4bd4597e84dd154ff93a28f
Gigi-G <seminara.luigi@gmail.com>
2021-03-13 03:13:11 +0100

    fix: scraper

---
+++ scraper/FormatPDF.py
@@ -0,0 +1,19 @@
+import pdftotext
+
+class FormatPDF:
+    
+    @staticmethod
+    def format_pdf(pathname) -> None:
+        with open(pathname, "rb") as f:
+            pdf = pdftotext.PDF(f)
+            f.close()
+        
+        formatted_lines = []
+        text = ""
+        for line in pdf:
+            text += line
+        split_text = text.split("\n")
+        for line in split_text:
+            formatted_lines.append(line.strip())
+        
+        return formatted_lines
