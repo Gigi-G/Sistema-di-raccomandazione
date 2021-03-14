daaefd33be1490e7bf74e0bfd5648b342ea04800
Gigi-G <seminara.luigi@gmail.com>
2021-03-14 10:26:33 +0100

    fix: keyboard
    - Right and Left button
    - Restart button

--- modules/utils/keyboard.py
+++ modules/utils/keyboard.py
@@ -1,24 +1,23 @@
 from telegram.ext.callbackcontext import CallbackContext
 from telegram.inline.inlinekeyboardbutton import InlineKeyboardButton
 
-
 def create_keyboard(context: CallbackContext, direction: int, subjects: dict) -> list:
-    keyboard_subject = []
-    context.user_data["index_list_subject"] += (3 * direction)
-    if(context.user_data["index_list_subject"] >= len(subjects)):
-        context.user_data["index_list_subject"] = len(subjects) - 1
-    i:int = 3 * direction
-    j:int = 0
-    for key, value in list(subjects.items())[context.user_data["index_list_subject"]:]:
-        if(j == i):
-            break
-        if(value in context.user_data["subject_names"]):
-            keyboard_subject.append([InlineKeyboardButton(value, callback_data=key + " - delete")])
-            j += direction
-    if(context.user_data["index_list_subject"] - 3 == 0):
-        keyboard_subject.append([InlineKeyboardButton("⏩", callback_data="RIGHT")])
-    elif(context.user_data["index_list_subject"] >= len(context.user_data["subject_names"]) - 3):
-        keyboard_subject.append([InlineKeyboardButton("⏪", callback_data="LEFT")])
-    else:
-        keyboard_subject.append([InlineKeyboardButton("⏪", callback_data="LEFT"), InlineKeyboardButton("⏩", callback_data="RIGHT")])
+    keyboard_subject: list = []
+    list_length = context.user_data["index_list_subject_length"]
+    context.user_data["index_list_subject"] += (list_length * direction)
+    if(context.user_data["index_list_subject"] >= len(context.user_data["subject_names"]) or context.user_data["index_list_subject"] <= -4):
+        context.user_data["index_list_subject"] -= (list_length * direction)
+    start_index = context.user_data["index_list_subject"]
+    end_index = start_index + list_length
+
+    for key in context.user_data["subject_names"][start_index:end_index]:
+        keyboard_subject.append([InlineKeyboardButton(subjects[key], callback_data=key + " - delete")])
+    
+    button: list = []
+    if start_index > 0:
+        button.append(InlineKeyboardButton("⏪", callback_data="LEFT"))
+    if end_index < len(context.user_data["subject_names"]):
+        button.append(InlineKeyboardButton("⏩", callback_data="RIGHT"))
+    keyboard_subject.append(button)
+    keyboard_subject.append([InlineKeyboardButton("🔁 RESTART 🔁", callback_data="restart")])
     return keyboard_subject
